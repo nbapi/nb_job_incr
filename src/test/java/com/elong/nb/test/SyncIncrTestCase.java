4f55dcb9c8d7d7ea260650598474fc43c9b37c51
苏红涛 <hongtao.su@corp.elong.com>
2016-10-09 19:37:13 +0800

    testcase

--- src/test/java/com/elong/nb/test/SyncIncrTestCase.java
+++ src/test/java/com/elong/nb/test/SyncIncrTestCase.java
@@ -2,24 +2,22 @@ package com.elong.nb.test;
 
 import org.junit.Test;
 
-import com.elong.nb.util.HttpClientUtils;
-
 public class SyncIncrTestCase {
 
 	@Test
 	public void testSyncHotelToDB() throws Exception {
 		// SyncHotelToDB,writeIncrOrderLog,SyncInventoryToDB,SyncRatesToDB,SyncStateToDB
-		String reqUrl = null;
+//		String reqUrl = null;
 		// reqUrl = "http://nbapi-syncincr.vip.elong.com/SyncHotelToDB";
 		// reqUrl = "http://nbapi-syncincr.vip.elong.com/writeIncrOrderLog";
 		// reqUrl = "http://nbapi-syncincr.vip.elong.com/SyncInventoryToDB";
 //		reqUrl = "http://nbapi-syncincr.vip.elong.com/SyncRatesToDB";
-		 reqUrl = "http://nbapi-syncincr.vip.elong.com/SyncStateToDB";
-		try {
-			String result = HttpClientUtils.httpPost(reqUrl, "", "application/json");
-			System.out.println(result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		 reqUrl = "http://nbapi-syncincr.vip.elong.com/SyncStateToDB";
+//		try {
+//			String result = HttpClientUtils.httpPost(reqUrl, "", "application/json");
+//			System.out.println(result);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 }
