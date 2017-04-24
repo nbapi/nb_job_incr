5e7734fa25d17911eed3f3caa361d8e45f56b216
苏红涛 <hongtao.su@corp.elong.com>
2017-04-24 18:38:34 +0800

    checklist 修改

--- src/main/java/com/elong/nb/checklist/ChecklistAspect.java
+++ src/main/java/com/elong/nb/checklist/ChecklistAspect.java
@@ -23,19 +23,22 @@ public class ChecklistAspect {
 	 * @throws Throwable
 	 */
 	public void handlerLogBefore(JoinPoint point) {
-		ThreadLocalUtil.set(point.toString() + "_" + ELONG_REQUEST_STARTTIME, System.currentTimeMillis());
-		RequestAttributes request = RequestContextHolder.getRequestAttributes();
-		// 调用链新起的线程，拦截方法会在此返回
-		if (request == null)
-			return;
+		try {
+			ThreadLocalUtil.set(point.toString() + "_" + ELONG_REQUEST_STARTTIME, System.currentTimeMillis());
+			RequestAttributes request = RequestContextHolder.getRequestAttributes();
+			// 调用链新起的线程，拦截方法会在此返回
+			if (request == null)
+				return;
 
-		// 1、Controller 2、调用链只有一个线程的所有拦截方法
-		Object guid = request.getAttribute(Constants.ELONG_REQUEST_REQUESTGUID, ServletRequestAttributes.SCOPE_REQUEST);
-//
-		if (guid == null) {// Controller请求回到此处
-			guid = UUID.randomUUID().toString();
-			request.setAttribute(Constants.ELONG_REQUEST_REQUESTGUID, guid, ServletRequestAttributes.SCOPE_REQUEST);
-			ThreadLocalUtil.set(Constants.ELONG_REQUEST_REQUESTGUID, guid);
+			// 1、Controller 2、调用链只有一个线程的所有拦截方法
+			Object guid = request.getAttribute(Constants.ELONG_REQUEST_REQUESTGUID, ServletRequestAttributes.SCOPE_REQUEST);
+			//
+			if (guid == null) {// Controller请求回到此处
+				guid = UUID.randomUUID().toString();
+				request.setAttribute(Constants.ELONG_REQUEST_REQUESTGUID, guid, ServletRequestAttributes.SCOPE_REQUEST);
+				ThreadLocalUtil.set(Constants.ELONG_REQUEST_REQUESTGUID, guid);
+			}
+		} catch (Exception e) {
 		}
 	}
 
@@ -46,32 +49,38 @@ public class ChecklistAspect {
 	 * @throws Throwable
 	 */
 	public void handlerLogAfter(JoinPoint point, Object returnValue) {
-		String classFullName = ClassUtils.getShortClassName(point.getSignature().getDeclaringTypeName());
-		String methodName = point.getSignature().getName();
-		long start = (Long) ThreadLocalUtil.get(point.toString() + "_" + ELONG_REQUEST_STARTTIME);
-		float useTime = System.currentTimeMillis() - start;
-		Object guid = ThreadLocalUtil.get(Constants.ELONG_REQUEST_REQUESTGUID);
-		if (guid == null)
-			guid = UUID.randomUUID().toString();
+		try {
+			String classFullName = ClassUtils.getShortClassName(point.getSignature().getDeclaringTypeName());
+			String methodName = point.getSignature().getName();
+			long start = (Long) ThreadLocalUtil.get(point.toString() + "_" + ELONG_REQUEST_STARTTIME);
+			float useTime = System.currentTimeMillis() - start;
+			Object guid = ThreadLocalUtil.get(Constants.ELONG_REQUEST_REQUESTGUID);
+			if (guid == null)
+				guid = UUID.randomUUID().toString();
 
-		ActionLogHelper.businessLog((String) guid, true, methodName, classFullName, null, useTime, 0, null, returnValue, point.getArgs());
+			ActionLogHelper.businessLog((String) guid, true, methodName, classFullName, null, useTime, 0, null, returnValue,
+					point.getArgs());
+		} catch (Exception e) {
+		}
 	}
 
 	public void handlerLogThrowing(JoinPoint point, Object throwing) {
-		String classFullName = ClassUtils.getShortClassName(point.getSignature().getDeclaringTypeName());
-		String methodName = point.getSignature().getName();
-		long start = (Long) ThreadLocalUtil.get(point.toString() + "_" + ELONG_REQUEST_STARTTIME);
-		float useTime = System.currentTimeMillis() - start;
-		Object guid = ThreadLocalUtil.get(Constants.ELONG_REQUEST_REQUESTGUID);
-		if (guid == null)
-			guid = UUID.randomUUID().toString();
+		try {
+			String classFullName = ClassUtils.getShortClassName(point.getSignature().getDeclaringTypeName());
+			String methodName = point.getSignature().getName();
+			long start = (Long) ThreadLocalUtil.get(point.toString() + "_" + ELONG_REQUEST_STARTTIME);
+			float useTime = System.currentTimeMillis() - start;
+			Object guid = ThreadLocalUtil.get(Constants.ELONG_REQUEST_REQUESTGUID);
+			if (guid == null)
+				guid = UUID.randomUUID().toString();
 
-		Exception e = null;
-		if (throwing instanceof Exception) {
-			e = (Exception) throwing;
+			if (throwing instanceof Exception) {
+				Exception e = (Exception) throwing;
+				ActionLogHelper.businessLog((String) guid, false, methodName, classFullName, e, useTime, -1, e.getMessage(), null,
+						point.getArgs());
+			}
+		} catch (Exception e) {
 		}
-
-		ActionLogHelper.businessLog((String) guid, false, methodName, classFullName, e, useTime, -1, e.getMessage(), null, point.getArgs());
 	}
 
 }
