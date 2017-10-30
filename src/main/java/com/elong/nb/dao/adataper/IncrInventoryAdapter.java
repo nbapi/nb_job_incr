57f9952d1c62ad29b45503804c04aaf63b0be638
苏红涛 <hongtao.su@corp.elong.com>
2017-10-30 18:15:23 +0800

    库存增量及时确认相关值入库

--- src/main/java/com/elong/nb/dao/adataper/IncrInventoryAdapter.java
+++ src/main/java/com/elong/nb/dao/adataper/IncrInventoryAdapter.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.elong.hotel.searchagent.thrift.dss.GetInvAndInstantConfirmResponse;
 import com.elong.hotel.searchagent.thrift.dss.InvDetail;
 import com.elong.hotel.searchagent.thrift.dss.MhotelDetail;
@@ -17,8 +19,8 @@ import com.elong.nb.model.bean.IncrInventory;
 
 public class IncrInventoryAdapter {
 
-	public Map<String,List<IncrInventory>> toNBObect(GetInvAndInstantConfirmResponse response) {
-		Map<String,List<IncrInventory>> incrInventoryMap = new HashMap<String, List<IncrInventory>>();
+	public Map<String, List<IncrInventory>> toNBObect(GetInvAndInstantConfirmResponse response) {
+		Map<String, List<IncrInventory>> incrInventoryMap = new HashMap<String, List<IncrInventory>>();
 		if (response != null) {
 			if (response.getReturn_code() == 0) {
 				if (response.getMhotel_detail() != null && response.getMhotel_detail().size() > 0) {
@@ -31,7 +33,7 @@ public class IncrInventoryAdapter {
 										String roomTypeID = SafeConvertUtils.ToRoomId(sroom.sroom_id);
 										String key = hotelCode + "|" + roomTypeID;
 										List<IncrInventory> incrInventorys = incrInventoryMap.get(key);
-										if(incrInventorys == null){
+										if (incrInventorys == null) {
 											incrInventorys = new ArrayList<IncrInventory>();
 										}
 										if (sroom != null && sroom.getInv_detail() != null && sroom.getInv_detail().size() > 0) {
@@ -48,6 +50,29 @@ public class IncrInventoryAdapter {
 												inv.setStatus(item.getStatus() == 0);// 0:有效
 												inv.setOverBooking(item.getIs_over_booking());// 0:可超售 1:不可超售
 												inv.setHotelCode(hotelCode);
+												inv.setIsInstantConfirm(item.instant_confirm);// 0:立即确认 1:非立即确认
+												String icBeginTime = item.getIc_begin_time();
+												String icEndTime = item.getIc_end_time();
+												// 不是立即确认情况下的兼容
+												if (!item.instant_confirm) {
+													if (inv.isStatus()) {
+														if (StringUtils.isEmpty(icBeginTime)) {
+															icBeginTime = "00:00";
+														}
+														if (StringUtils.isEmpty(icEndTime)) {
+															icEndTime = "23:59";
+														}
+													} else {
+														if (StringUtils.isEmpty(icBeginTime)) {
+															icBeginTime = "23:59";
+														}
+														if (StringUtils.isEmpty(icEndTime)) {
+															icEndTime = "00:00";
+														}
+													}
+												}
+												inv.setIC_BeginTime(icBeginTime);
+												inv.setIC_EndTime(icEndTime);
 												convertInventory(inv);
 												incrInventorys.add(inv);
 											}
